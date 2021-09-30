43f0af713f6477b85720fce9f6b058b98bc09164
weijia_cheng1 <weijia_cheng1@baylor.edu>
2021-09-29 20:18:31 -0500

    9/29 2nd

--- alien_invasion.py
+++ alien_invasion.py
@@ -18,14 +18,26 @@ class AlienInvasion:
 
     def run_game(self):
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
             
-            # redraw every loop
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            pygame.display.flip()
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.rect.x += 1
+    
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
+
+        
 
 if __name__ == '__main__':
     ai = AlienInvasion()
