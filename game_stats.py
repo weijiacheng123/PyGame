2314c4057b67e6198b559bfe1fad5f1750cd65be
weijiacheng123 <weijia_cheng1@baylor.edu>
2021-10-24 21:45:37 -0500

    Final Change

--- game_stats.py
+++ game_stats.py
@@ -5,9 +5,10 @@ class GameStats:
         self.reset_stats()
         self.game_active = False
         self.high_score = 0
-        self.level = 1
+        
 
 
     def reset_stats(self):
         self.ships_left = self.settings.ship_limit
         self.score = 0
+        self.level = 1
